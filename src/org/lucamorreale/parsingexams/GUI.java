f4e59c7d7a122f96302ebbeacdf783aa954138f8
Luca Morreale <luca.morreale93@gmail.com>
2015-03-24 16:50:04 +0100

    Created custom menu bar

--- src/org/lucamorreale/parsingexams/GUI.java
+++ src/org/lucamorreale/parsingexams/GUI.java
@@ -3,23 +3,41 @@
  */
 package org.lucamorreale.parsingexams;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
 /**
  * @author Luca Morreale
  *
  */
-public class GUI extends JFrame{
+public final class GUI extends JFrame implements ActionListener{
+    private static final long serialVersionUID = 3194012730957326358L;
 
     public GUI(){
 
-        setTitle("Academic Exams Parser");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setDefaultLookAndFeelDecorated(true);
+        this.setTitle("Academic Exams Parser");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.setDefaultLookAndFeelDecorated(true);
+
+        this.setLayout(new BorderLayout(0, 0));
+
+        MainWindowMenu menuBar = new MainWindowMenu(this);
+        setJMenuBar(menuBar);
+
+        this.pack();
+        this.setLocationRelativeTo(null);
 
 
-        pack();
-        setLocationRelativeTo(null);
+        //JMenuBar menuBar = new JMenuBar();
+        //getContentPane().add(menuBar, BorderLayout.NORTH);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        // TODO Auto-generated method stub
+
     }
 
 }
---
+++ src/org/lucamorreale/parsingexams/MainWindowMenu.java
@@ -0,0 +1,59 @@
+/**
+ *
+ */
+package org.lucamorreale.parsingexams;
+
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ * @author Luca Morreale
+ *
+ */
+public final class MainWindowMenu extends JMenuBar{
+    private static final long serialVersionUID = -4760379938611375664L;
+
+    private ActionListener actionToCall;
+
+    public MainWindowMenu(ActionListener listenerToCall){
+
+        this.actionToCall = listenerToCall;
+
+        buildFile();
+        buildHelp();
+    }
+
+    private void buildFile(){
+
+        JMenu mFile = new JMenu("File");
+        JMenuItem itemExit = new JMenuItem("Exit");
+
+        itemExit.addActionListener(EventHandler.create(ActionListener.class, this.actionToCall, "exit"));    // at row
+        mFile.addSeparator();
+        mFile.add(itemExit);
+
+        this.add(mFile);
+
+    }
+
+    private void buildHelp(){
+        JMenu mHelp = new JMenu("Help");
+        JMenuItem itemBug = new JMenuItem("Bug");
+
+        itemBug.addActionListener(EventHandler.create(ActionListener.class, this, "startBugDialog"));    // at row
+        mHelp.add(itemBug);
+
+        this.add(mHelp);
+    }
+
+    @SuppressWarnings("unused")
+    private void starBugDialog() {
+
+    }
+
+
+}
