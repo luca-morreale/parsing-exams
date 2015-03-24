955f66f14b4759679d3d6e6a11a77e98fef1df12
Luca Morreale <luca.morreale93@gmail.com>
2015-03-24 14:02:28 +0100

    Created main class

---
+++ src/org/lucamorreale/parsingexams/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package org.lucamorreale.parsingexams;
+
+/**
+ * @author Luca Morreale
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+
+}
