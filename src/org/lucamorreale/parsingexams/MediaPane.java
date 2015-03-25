879f865025171664874040b52712dcc95244ed9c
Luca Morreale <luca.morreale93@gmail.com>
2015-03-25 17:21:00 +0100

    Created top panel of first tab and changed refernce for resource folder

--- src/org/lucamorreale/parsingexams/MediaPane.java
+++ src/org/lucamorreale/parsingexams/MediaPane.java
@@ -4,7 +4,9 @@
 package org.lucamorreale.parsingexams;
 
 import java.awt.BorderLayout;
+import java.util.logging.Logger;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -16,11 +18,23 @@ public final class MediaPane extends JPanel {
     private static final long serialVersionUID = 4619821091873343199L;
 
     private MediaTable tMedia;
+    private PlainButton addBtn;
+    private PlainButton removeBtn;
+    private PlainButton exportBtn;
+
+
 
     public MediaPane(){
         super();
 
         this.setLayout(new BorderLayout());
+
+
+        addBtn = new PlainButton("Aggiungi Corso");
+        removeBtn = new PlainButton("Elimina Corso");
+        exportBtn = new PlainButton("Esporta Tabella");
+        generateTopPane();
+
 
         tMedia = new MediaTable();
         JScrollPane scroll = new JScrollPane();
@@ -29,5 +43,26 @@ public final class MediaPane extends JPanel {
 
         this.add(scroll, BorderLayout.CENTER);
     }
+
+    private void generateTopPane(){
+
+        JPanel topPane = new JPanel();
+
+        try{
+            addBtn.setIcon(new ImageIcon(Main.class.getResource("/resources/images/add.png")));
+            removeBtn.setIcon(new ImageIcon(GUI.class.getResource("/resources/images/delete_all.png")));
+            exportBtn.setIcon(new ImageIcon(GUI.class.getResource("/resources/images/save.png")));
+        } catch(NullPointerException exc) {
+            LOG.severe(PlainButton.class +" resources not found: "+ exc.getMessage());
+        }
+
+        topPane.add(addBtn);
+        topPane.add(removeBtn);
+        topPane.add(exportBtn);
+
+        this.add(topPane, BorderLayout.NORTH);
+    }
+
+    private static final Logger LOG = Logger.getLogger(MediaPane.class.getName());
 
 }
