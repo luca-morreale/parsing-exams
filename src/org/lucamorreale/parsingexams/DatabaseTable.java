f6d7a2d158ac52acf432e394247c459a5408ff4a
Luca Morreale <luca.morreale93@gmail.com>
2015-04-02 16:30:50 +0200

    Adapt constructor to a less finalized table

--- src/org/lucamorreale/parsingexams/DatabaseTable.java
+++ src/org/lucamorreale/parsingexams/DatabaseTable.java
@@ -3,6 +3,10 @@
  */
 package org.lucamorreale.parsingexams;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.util.List;
+
 import javax.swing.JTable;
 
 /**
@@ -12,9 +16,42 @@ import javax.swing.JTable;
 public class DatabaseTable extends JTable {
     private static final long serialVersionUID = -6102001466807223180L;
 
+    private SQLiteManager db;
+    protected KeyTableModel model;
+    protected TablePopupMenu popupMenu;
 
-    public DatabaseTable(){
+    private final String DB_TABLE;
+    private final List<String> Columns;
+
+    public DatabaseTable(List<String> columns, String db_table){
         super();
 
+        DB_TABLE = db_table;
+        Columns = columns;
+
+        this.model = new KeyTableModel();
+        for (String c : Columns) {
+            model.addColumn(c);
+        }
+
+        popupMenu = new TablePopupMenu(null);
+        init();
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                db = new SQLiteManager("jdbc:sqlite:data/source.sqlite");
+            }
+        });
+
     }
+    private void init(){
+        model.addTableModelListener(this);
+        this.setComponentPopupMenu(popupMenu);
+        this.setInheritsPopupMenu(true);
+
+        this.setModel(this.model);
+        this.setBackground(Color.WHITE);
+    }
+
 }
