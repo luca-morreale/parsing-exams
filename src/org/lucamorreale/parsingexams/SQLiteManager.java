7bbb45266c3dfc868d4526517f15fc2262055162
Luca Morreale <luca.morreale93@gmail.com>
2015-04-02 23:54:25 +0200

    Added method to get an entire row without iterate over fields

--- src/org/lucamorreale/parsingexams/SQLiteManager.java
+++ src/org/lucamorreale/parsingexams/SQLiteManager.java
@@ -330,7 +330,28 @@ public final class SQLiteManager {
         }
     }
 
+    /**
+     * Iterate over all column selected and return an array of Object
+     * containing the data of the current row.
+     * @return
+     */
+    public Object[] getRowArray(){
 
+        try {
+
+            int colNumber = rs.getMetaData().getColumnCount();
+            Object[] row = new Object[colNumber];
+            for(int i=0; i<colNumber; i++){
+                row[i] = getField(i);
+            }
+            return row;
+
+        } catch (SQLException e) {
+            LOG.severe(e.getMessage() + " getRowArray()");
+            return null;
+        }
+
+    }
 
     /**
      *
