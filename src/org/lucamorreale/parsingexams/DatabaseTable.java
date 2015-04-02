f8fc408d2cdee8b769eb887f16fde03b01511cbc
Luca Morreale <luca.morreale93@gmail.com>
2015-04-02 13:42:21 +0200

    Init new base for final table

---
+++ src/org/lucamorreale/parsingexams/DatabaseTable.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package org.lucamorreale.parsingexams;
+
+import javax.swing.JTable;
+
+/**
+ * @author Luca Morreale
+ *
+ */
+public class DatabaseTable extends JTable {
+    private static final long serialVersionUID = -6102001466807223180L;
+
+
+    public DatabaseTable(){
+        super();
+
+    }
+}
